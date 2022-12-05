0a144049b0dad247549e80b60425186641d83b7b
Pranshu <sahupranshu637@gmail.com>
2022-12-05 20:32:34 +0530

    add footer

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        pink: "hsl(322, 100%, 66%)",
+        paleCyan: "hsl(193, 100%, 96%)",
+        darkCyan: "hsl(192, 100%, 9%)",
+        grayishBlue: "hsl(208, 11%, 55%)",
+      },
+      fontFamily: {
+        openSans: ["Open Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
